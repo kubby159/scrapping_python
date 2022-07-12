27c34c4d03b503d354cf3c1b3b418f28a8ac3119
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-12 13:41:53 +0900

    python challenge

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import requests as req
+from bs4 import BeautifulSoup as bs
+indeed_result  =  req.get('https://kr.indeed.com/jobs?q=%EC%A4%91%EC%86%8C%EA%B8%B0%EC%97%85&l=%EC%9D%B8%EC%B2%9C&vjk=31d2f80f34bb3961')
+# indeed_result.text : html 가져오기
+
+soup = bs(indeed_result.text, 'html.parser')
+pagination = soup.find('div',{'class' : 'pagination'})
+
+pages = pagination.find_all('a')
+
+spans = []
+for page in pages:
+    spans.append(page.find('span'))
 
 
-from indeed import extract_indeed_pages, extract_indeed_jobs
-
-last_indeed_page = extract_indeed_pages()
-
-
-
-
-indeed_jobs =  extract_indeed_jobs(last_indeed_page)
-
-
-
-
+print(spans[:-1])
