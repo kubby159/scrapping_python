bfbc9cc84301c05564916887307f1dc182c4a7c4
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-07-19 22:56:57 +0900

    Ineed scrapping done

--- indeed.py
+++ indeed.py
@@ -24,26 +24,39 @@ def extract_indeed_pages():
 
 
 
+def extract_job(html):
+        
+        title = html.find('h2',{'class': 'jobTitle'}).string
+        company = html.find('span',{'class': 'companyName'})
+        company_anchor = company.find('a')
+        location = html.find('div', {'class': 'companyLocation'})
+        if company_anchor is not None:
+            company = str(company_anchor.string)
+        else:
+            company  = company.string
+
+        if location.string is not None:
+            location = '정보없음'
+        else:
+            location = location.string
+
+
+        return {"job_title" : title, "company_name": company, 'location' : location}
+    
+
+
+
 def extract_indeed_jobs(last_page):
     jobs = []
-    # for page in range(last_page):
-    result =req.get(f'{INDEED_URL}&start={0*LIMIT}')
-    soup = bs(result.text,'html.parser')
-    results = soup.find_all('td',{'class':'resultContent'})
+    for page in range(last_page):
+        print(f'Scrapping  page : {page}')
+        result =req.get(f'{INDEED_URL}&start={last_page*LIMIT}')
+        soup = bs(result.text,'html.parser')
+        results = soup.find_all('td',{'class':'resultContent'})
     
-    for result in results:
-        title = result.find('h2',{'class': 'jobTitle'})
-        company = result.find('span',{'class': 'companyName'})
-        if company.find('a') is not None:
-            print(company.find('a').string)
-        else:
-            print(company.string)
-    
-      
-      
-
-    
-   
+        for result in results:
+            job  = extract_job(result)
+            jobs.append(job)
 
     
     return jobs
